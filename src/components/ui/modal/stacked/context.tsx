c96dba314c7a0d110e12e9c5c4f9ac9b7bdf7c36
lkieryan <lqren7@gmail.com>
2025-07-16 19:24:00 +0800

    feat: implement Modal stacked context management

---
+++ src/components/ui/modal/stacked/context.tsx
@@ -0,0 +1,48 @@
+import type { FC, RefObject } from "react"
+import { createContext as reactCreateContext } from "react"
+import { createContext as createContextSelector } from "use-context-selector"
+
+import type { ModalProps } from "./types"
+
+export type CurrentModalContentProps = ModalActionsInternal & {
+  ref: RefObject<HTMLElement | null>
+  modalElementRef: RefObject<HTMLElement | null>
+}
+
+const warnNoProvider = () => {
+  if (import.meta.env.DEV) {
+    console.error(
+      "No ModalProvider found, please make sure to wrap your component with ModalProvider",
+    )
+  }
+}
+const defaultCtxValue: CurrentModalContentProps = {
+  dismiss: warnNoProvider,
+  setClickOutSideToDismiss: warnNoProvider,
+  ref: { current: null },
+  modalElementRef: { current: null },
+  getIndex: () => 0,
+}
+
+export const CurrentModalContext = reactCreateContext<CurrentModalContentProps>(defaultCtxValue)
+export const CurrentModalStateContext = createContextSelector<{
+  isTop: boolean
+  isInModal: boolean
+}>({
+  isTop: true,
+  isInModal: false,
+})
+
+export type ModalContentComponent<T = object> = FC<ModalActionsInternal & T>
+export type ModalActionsInternal = {
+  dismiss: () => void
+  setClickOutSideToDismiss: (value: boolean) => void
+  getIndex: () => number
+}
+
+type Disposer = () => void
+type PresentModalContextInternalFn = (props: ModalProps & { id?: string }) => Disposer
+export const PresentModalContextInternal = reactCreateContext<PresentModalContextInternalFn>(() => {
+  warnNoProvider()
+  return () => {}
+})

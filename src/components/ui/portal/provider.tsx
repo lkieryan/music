ea71bc004e82ef14000662e9aad52eeaf9f31148
lkieryan <lqren7@gmail.com>
2025-07-17 16:37:00 +0800

    feat: create Portal component for rendering outside DOM tree

---
+++ src/components/ui/portal/provider.tsx
@@ -0,0 +1,9 @@
+import { createContext, use } from "react"
+
+export const useRootPortal = () => {
+  const ctx = use(RootPortalContext)
+
+  return ctx || document.body
+}
+
+export const RootPortalContext = createContext<HTMLElement | undefined>(undefined)

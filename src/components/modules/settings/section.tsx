334ebdf747e8103bcfc5d32bbfd5308cba300d21
lkieryan <lqren7@gmail.com>
2025-07-24 17:16:00 +0800

    feat: create settings control and section components

---
+++ src/components/modules/settings/section.tsx
@@ -0,0 +1,68 @@
+/* eslint-disable @eslint-react/no-children-to-array */
+/* eslint-disable @eslint-react/no-children-map */
+
+import { cn } from "~/lib/helper"
+import type { FC, PropsWithChildren, ReactNode } from "react"
+import { cloneElement } from "react"
+import * as React from "react"
+
+import { SettingActionItem, SettingDescription, SettingSwitch } from "./control"
+
+export const SettingSectionTitle: FC<{
+  title: string | ReactNode
+
+  margin?: "compact" | "normal"
+}> = ({ title, margin }) => (
+  <div
+    className={cn(
+      "text-text text-headline shrink-0 font-bold capitalize opacity-50 first:mt-0",
+      margin === "compact" ? "mb-2 mt-8" : "mb-4 mt-10",
+    )}
+  >
+    {title}
+  </div>
+)
+
+export const SettingItemGroup: FC<PropsWithChildren> = ({ children }) => {
+  const childrenArray = React.Children.toArray(children)
+  return React.Children.map(children, (child, index) => {
+    if (typeof child !== "object") return child
+
+    if (child === null) return child
+
+    const compType = (child as React.ReactElement).type
+    if (compType === SettingDescription) {
+      const prevIndex = index - 1
+      const prevChild = childrenArray[prevIndex]
+      const prevType = getChildType(prevChild)
+
+      switch (prevType) {
+        case SettingSwitch: {
+          return cloneElement(child as React.ReactElement, {
+            // @ts-expect-error
+            className: "!-mt-2",
+          })
+        }
+        case SettingActionItem: {
+          return cloneElement(child as React.ReactElement, {
+            // @ts-expect-error
+            className: "!-mt-2",
+          })
+        }
+        default: {
+          return child
+        }
+      }
+    }
+
+    return child
+  })
+}
+
+const getChildType = (child: ReactNode) => {
+  if (typeof child !== "object") return null
+
+  if (child === null) return null
+
+  return (child as React.ReactElement).type
+}

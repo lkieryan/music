df50b55ddb219c83525b5c373fe536d0362478b9
lkieryan <lqren7@gmail.com>
2025-07-27 11:19:00 +0800

    feat: implement settings page architecture

---
+++ src/pages/settings/layout.tsx
@@ -0,0 +1,3 @@
+// NOTE: we disable directly nav to setting routes.
+// because we need to open the setting modal in the main window when the main window exists,
+export const Component = () => null

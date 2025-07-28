bbec597c7fd69c14fe17ae26b156552b941387ba
lkieryan <lqren7@gmail.com>
2025-07-28 08:24:00 +0800

    feat: create internationalization provider

---
+++ src/providers/root/i18n-provider.tsx
@@ -0,0 +1,13 @@
+import { useAtom } from "jotai"
+import type { FC, PropsWithChildren } from "react"
+import { I18nextProvider } from "react-i18next"
+
+import { i18nAtom } from "~/i18n"
+
+export const I18nProvider: FC<PropsWithChildren> = ({ children }) => {
+  const [currentI18NInstance, update] = useAtom(i18nAtom)
+
+  
+
+  return <I18nextProvider i18n={currentI18NInstance}>{children}</I18nextProvider>
+}

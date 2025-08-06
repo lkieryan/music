b658dc535eee8fc879a7c215bf09062e0ce7a9e5
lkieryan <lqren7@gmail.com>
2025-08-06 16:32:00 +0800

    feat: create plugin system architecture

---
+++ plugins/vite-ast.ts
@@ -0,0 +1,10 @@
+import { RemoveWrapperFunction } from "unplugin-ast/transformers"
+import AST from "unplugin-ast/vite"
+
+export const astPlugin = AST({
+  transformer: [
+    RemoveWrapperFunction([
+      "tw",
+    ]),
+  ],
+})

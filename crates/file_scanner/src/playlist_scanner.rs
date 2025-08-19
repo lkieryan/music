a8a8036c5b1435b99935d04c2f86b4f60b644149
lkieryan <lqren7@gmail.com>
2025-08-19 20:42:15 +0800

    feat: enhance scanner modules and Android support

--- crates/file_scanner/src/playlist_scanner.rs
+++ crates/file_scanner/src/playlist_scanner.rs
@@ -1,19 +1,3 @@
-// Moosync
-// Copyright (C) 2024, 2025  Moosync <support@moosync.app>
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 use std::{
     fs::{self, File},
     io::{self, BufRead},
@@ -28,7 +12,7 @@ use types::{
 };
 
 use substring::Substring;
-use types::errors::{MoosyncError, Result};
+use types::errors::{MusicError, Result};
 
 use uuid::Uuid;
 
@@ -246,7 +230,7 @@ impl<'a> PlaylistScanner<'a> {
             let playlist_scan_res = self.scan_playlist(&playlist);
             if playlist_scan_res.is_err() {
                 tx_playlist
-                    .send(Err(MoosyncError::String(format!(
+                    .send(Err(MusicError::String(format!(
                         "Failed to scan {}: {:?}",
                         playlist.display(),
                         playlist_scan_res.unwrap_err()

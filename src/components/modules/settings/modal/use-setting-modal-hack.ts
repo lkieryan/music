cd1aaa2a6a9ba1f755c87131e44311dd5342a057
lkieryan <lqren7@gmail.com>
2025-08-22 20:58:19 +0800

    refactor: clean up deprecated settings hooks

--- src/components/modules/settings/modal/use-setting-modal-hack.ts
+++
@@ -1,3 +0,0 @@
-// HACK: Use expose the navigate function in the window object, avoid to import `router` circular issue.
-// eslint-disable-next-line @eslint-react/hooks-extra/ensure-custom-hooks-using-other-hooks
-export const useSettingModal = () => window.router.showSettings
